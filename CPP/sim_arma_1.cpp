5ea1f50bb1376cc4bb15116ea535d05598d2f322
hmnssato <hmns.sato@gmail.com>
2026-01-23 09:51:56 +0900

    Update sim_arma_1.cpp
    Some corrects for FENE

--- CPP/sim_arma_1.cpp
+++ CPP/sim_arma_1.cpp
@@ -61,7 +61,7 @@ NumericVector rcpp_sim(int imax, double kk, int aloc, int bloc, int bound,
           xi = abs(xx(i-1,l+1)-xx(i-1,l));
           dphi[l]=kk*abs(xi-ssr);
           if (xi < pow(2,1/6) * ssr){
-            dphi[l]=dphi[l]+epsilon1*(4*kbt*(-12*pow(ssr,12)/pow(xi,13)+6*pow(ssr,6)/pow(xi,7)));
+            dphi[l]=dphi[l]+epsilon1*(4*kbt*(-12*pow(ssr,12)/pow(xi,13)+6*pow(ssr,6)/pow(xi,7))+30*kbt*xi/(1-pow(xi/1.6/ssr,2));
           }
         }
       }
